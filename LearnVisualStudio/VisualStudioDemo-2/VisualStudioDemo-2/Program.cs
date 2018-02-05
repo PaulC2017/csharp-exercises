e201dc8786e51806baf4dad69a03127391c3b8b4
PaulC2017 <paulc2017@att.net>
2018-02-05 12:26:17 -0800

    One more test

--- LearnVisualStudio/VisualStudioDemo-2/VisualStudioDemo-2/Program.cs
+++ LearnVisualStudio/VisualStudioDemo-2/VisualStudioDemo-2/Program.cs
@@ -7,6 +7,8 @@ namespace VisualStudioDemo_2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Again World!");
+            Console.ReadLine();
+            // another test of  pushing to github
         }
     }
 }
