bbf8e3111fb46d9e47833d3954ad3da3d52e565b
PaulC2017 <paulc2017@att.net>
2018-02-05 12:21:31 -0800

    Initial commit - VisualStudioDemo-2

---
+++ LearnVisualStudio/VisualStudioDemo-2/VisualStudioDemo-2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VisualStudioDemo_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Again World!");
+        }
+    }
+}
