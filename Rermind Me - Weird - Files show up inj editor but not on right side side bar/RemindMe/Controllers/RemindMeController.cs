9a2979405da6dcc6a2acefb5f412776924299f68
PaulC2017 <paulc2017@att.net>
2018-05-14 21:52:07 -0400

    Added ability to display a users events and recurring reminders

--- Rermind Me - Weird - Files show up inj editor but not on right side side bar/RemindMe/Controllers/RemindMeController.cs
+++ Rermind Me - Weird - Files show up inj editor but not on right side side bar/RemindMe/Controllers/RemindMeController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using RemindMe.ViewModels;
@@ -8,6 +9,15 @@ using RemindMe.Models;
 using RemindMe.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Http;
+using static Microsoft.AspNetCore.Hosting.Internal.HostingApplication;
+using System.Text;
+using Microsoft.EntityFrameworkCore.Storage;
+using System.Collections.ObjectModel;
+using System.Collections;
+using System.Dynamic;
+
+
+
 
 namespace RemindMe.Controllers
 {
@@ -60,8 +70,9 @@ namespace RemindMe.Controllers
                 context.User.Add(newUser);
                 context.SaveChanges();
                 ViewBag.User = registerUserViewModel;
-                ViewData["currentUser"] = registerUserViewModel.Username;
-                HttpContext.Session.SetString("Username", registerUserViewModel.Username);
+                
+                HttpContext.Session.SetString("Username", newUser.Username);
+                HttpContext.Session.SetInt32("ID", newUser.ID);
                 return View("UserHomePage", newUser);
                 
             }
@@ -99,7 +110,8 @@ namespace RemindMe.Controllers
                     return View(userLoginViewModel);
                 }
 
-                HttpContext.Session.SetString("Username", userLoginViewModel.Username);
+                HttpContext.Session.SetString("Username", checkUserLogInInfo.Username);  //capture Username for use elsewhere in this app
+                HttpContext.Session.SetInt32("ID", checkUserLogInInfo.ID);  //capture ID for use elsewhere in this app
                 return View("UserHomePage", checkUserLogInInfo);
             }
 
@@ -160,15 +172,81 @@ namespace RemindMe.Controllers
                 return View(newEventAndReminder);
         }
 
-        /*public IActionResult ViewEventsAndReminders()
+        public IActionResult Test()
         {
            if ( HttpContext.Session.GetString("Username")  == "")
               {
                 return View("Index");
               }
 
+            /*
+            // Generate List of Recurring Reminders
+            IList < RecurringReminders > recurringReminders =
+                               context.RecurringReminders.Include(rr => rr.User).ToList();
+            // reduce the list to those recurring reminders associated with the current user
+          
+
+            var userRecurringReminders = 
+                recurringReminders.Where(rr => rr.UserId == HttpContext.Session.GetInt32("ID"));
+           */
+
+
+
+
+            // retrieve only those records for the current user - a better way to do this 
+            //than the above commented out code 
+
+            //IList<RecurringReminders> userRecurringReminders = context.RecurringReminders.Include(u => u.User).ToList().Where(User == HttpContext.Session.GetString("Username") ) ;
+            //IList<RecurringReminders> userRecurringReminders = context.RecurringReminders.Include(u => u.User).ToList();
+            //IList<RecurringReminders> userRecurringReminders = context.RecurringReminders.Include(u => u.User).ToList();
+            var userRecurringReminders = from recurringReminder in context.RecurringReminders
+                                         where (recurringReminder.UserId == HttpContext.Session.GetInt32("ID"))
+                                         select new
+                                         {
+                                             recurringReminder.RecurringReminderName,
+                                             recurringReminder.RecurringReminderDescription,
+                                             recurringReminder.RecurringReminderStartAlertDate,
+                                             recurringReminder.RecurringReminderLastAlertDate,
+                                             recurringReminder.RecurringReminderRepeatFrequency
+                                         };
+
+           
+           
+
+           
+            return View(userRecurringReminders);
         }
-        */
+        
+       public ActionResult SingleUserRecurringEventsAndReminders()
+       {
+
+            if (HttpContext.Session.GetString("Username") == "")
+            {
+                return View("Index");
+            }
+
+
+            dynamic userRecurringReminders = (from ch in context.RecurringReminders
+                                              join
+                                              ca in context.User
+                                              on ch.UserId equals ca.ID
+                                              where ca.ID == HttpContext.Session.GetInt32("ID")
+                                              select new
+                                              {
+                                               ch.RecurringReminderName,
+                                               ch.RecurringReminderDescription,
+                                               ch.RecuringReminderCreateDate,
+                                               ch.RecurringReminderStartAlertDate,
+                                               ch.RecurringReminderLastAlertDate,
+                                               ch.RecurringReminderFirstAlertTime,
+                                               ch.RecurringReminderSecondAlertTime,
+                                               ch.RecuringReminderAlertFrequency,
+                                               ch.RecurringReminderRepeatFrequency
+                                              }).AsEnumerable().Select(c => c.ToExpando());
+            return View(userRecurringReminders);
+
+        }
+        
         public IActionResult UserLogout()
         {
             HttpContext.Session.SetString("Username", "");
@@ -178,6 +256,11 @@ namespace RemindMe.Controllers
 
         public IActionResult UserHomePage()
         {
+            if (HttpContext.Session.GetString("Username") == "")
+            {
+                return View("Index");
+            }
+
             User currentUser = context.User.Single(u => u.Username == HttpContext.Session.GetString("Username"));
             return View(currentUser);
         }
---
+++ Rermind Me - Weird - Files show up inj editor but not on right side side bar/RemindMe/Models/ExpandoStaticClass.cs
@@ -0,0 +1,19 @@
+﻿using System.Dynamic;
+using System.Web;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+
+namespace RemindMe.Models
+{
+    public static class ExpandoStaticClass
+    {
+        public static ExpandoObject ToExpando(this object anonymousObject)
+        {
+            IDictionary<string, object> anonymousDictionary = HtmlHelper.AnonymousObjectToHtmlAttributes(anonymousObject);
+            IDictionary<string, object> expando = new ExpandoObject();
+            foreach (var item in anonymousDictionary)
+                expando.Add(item);
+            return (ExpandoObject)expando;
+        }
+    }
+}
